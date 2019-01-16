d39039994c9d389f137ff6f96a40eaf79b304470
Krishnendu <30381516+KDDS@users.noreply.github.com>
2019-01-16 12:12:57 +1100

    UI and SERVER files
    Separate UI and SERVER

--- POCs/server.R
+++ POCs/server.R
@@ -1,7 +1,13 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
-library(RODBCext) #https://stackoverflow.com/questions/44502558/insert-multiple-rows-from-r-dataframe-into-oracle-database
-library(shinythemes)
-library(RODBC)
 
 connHandle <- odbcConnect("ORA_XE", uid="SYSTEM", pwd="1234")
 query <- "INSERT INTO RESPONSES (FIRST_NAME, LAST_NAME, IN_TIME, OUT_TIME, SYSTEM) VALUES (?, ?, ?, ?, ?)"
@@ -13,11 +19,7 @@ fieldsAll <-  c("First_Name", "Last_Name", "In_Time", "Out_Time", "System")
 fieldsMandatory <-
   c("First_Name", "Last_Name", "In_Time", "Out_Time")
 
-# add an asterisk to an input label
-labelMandatory <- function(label) {
-  tagList(label,
-          span("*", class = "mandatory_star"))
-}
+
 
 # get current Epoch time
 epochTime <- function() {
@@ -49,76 +51,8 @@ loadData <- function() {
 # directory where responses get stored
 responsesDir <- file.path("responses")
 
-# CSS to use in the app
-appCSS <-
-  ".mandatory_star { color: red; }
-.shiny-input-container { margin-top: 25px; }
-#submit_msg { margin-left: 15px; }
-#error { color: red; }
-body { background: #fcfcfc; }
-#header { background: #fff; border-bottom: 1px solid #ddd; margin: -20px -15px 0; padding: 15px 15px 10px; }
-"
 
-
-shinyApp(
-  ui = fluidPage(theme = shinytheme("sandstone"),
-    
-    navbarPage(
-      tabPanel("Navbar 0", "Performance Dashboard"),
-    tabPanel(
-      "Data Entry",
-      shinyjs::useShinyjs(),
-      shinyjs::inlineCSS(appCSS),
-      fluidRow(column(1),
-               column(6,
-                      h2(
-                        'Employee Login Details'
-                      ))),
-      
-      fluidRow(
-        br(),
-        column(1),
-        column(
-          4,
-          br(),
-          div(
-            id = "form",
-            textInput("First_Name", labelMandatory("First Name"), ""),
-            textInput("Last_Name", labelMandatory("Last Name")),
-            textInput("In_Time", labelMandatory("IN - Time"), ""),
-            textInput("Out_Time", labelMandatory("Out - Time"), ""),
-            radioButtons(
-              "System",
-              "System2",
-              choices = c('COSEC', 'NOBEL'),
-              inline = T
-            ),
-            #checkboxInput("COSEC", "Door Data", F),
-            actionButton("submit", "Submit", class = "btn-primary"),
-            
-            shinyjs::hidden(span(id = "submit_msg", "Submitting..."),
-                            div(id = "error",
-                                div(
-                                  br(), tags$b("Error: "), span(id = "error_msg")
-                                )))
-          ),
-          
-          shinyjs::hidden(div(
-            id = "thankyou_msg",
-            h3("Thanks, your response was submitted successfully!"),
-            actionLink("submit_another", "Submit another response")
-          ))
-        ),
-        column(3,
-               uiOutput("adminPanelContainer"))
-      )
-    ),
-    tabPanel("Reports", "Performance Reports"),
-    tabPanel("Visualisation", "Data Visualisation")
-  )),
-  
-  
-  server = function(input, output, session) {
+server = function(input, output, session) {
     # Enable the Submit button when all mandatory fields are filled out
     observe({
       mandatoryFilled <-
@@ -210,4 +144,3 @@ shinyApp(
       }
     )
   }
-)
